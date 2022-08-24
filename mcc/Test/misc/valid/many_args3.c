185b0647dd2ca39c611bdf84c4a4e61c5ab7b623
rumkugel13 <rumkugel13@outlook.com>
2022-08-24 19:03:31 +0200

    add more test cases

---
+++ mcc/Test/misc/valid/many_args3.c
@@ -0,0 +1,9 @@
+﻿int eleven(int one, int two, int three, int four, int five, int six, int seven, int eight, int nine, int ten, int eleven)
+{
+    return one + two + three + four + five + six + seven + eight + nine + ten + eleven;
+}
+
+int main()
+{
+    return eleven(1, eleven(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 3, 4, 5, 6, 7, 8, 9, 10, 11);
+}
